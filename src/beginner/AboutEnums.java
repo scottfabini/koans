5dbc4795f3af82bdfeee2539ebbf1fcc8c9324d6
Scott Fabini <sfabini@pdx.edu>
2016-06-27 15:33:14 -0700

    Completed Koans: AboutEnums

--- src/beginner/AboutEnums.java
+++ src/beginner/AboutEnums.java
@@ -9,22 +9,22 @@ public class AboutEnums {
 
 
     enum Colors {
-        Red, Blue, Green, Yellow // what happens if you add a ; here?
-        // What happens if you type Red() instead?
+        Red, Blue, Green, Yellow // what happens if you add a ; here? Unnecessary semicolon warning.
+        // What happens if you type Red() instead? No change.
     }
 
     @Koan
     public void basicEnums() {
         Colors blue = Colors.Blue;
-        assertEquals(blue == Colors.Blue, __);
-        assertEquals(blue == Colors.Red, __);
-        assertEquals(blue instanceof Colors, __);
+        assertEquals(blue == Colors.Blue, true);
+        assertEquals(blue == Colors.Red, false);
+        assertEquals(blue instanceof Colors, true);
     }
 
     @Koan
     public void basicEnumsAccess() {
         Colors[] colorArray = Colors.values();
-        assertEquals(colorArray[2], __);
+        assertEquals(colorArray[2], Colors.Green);
     }
 
     enum SkatSuits {
@@ -40,9 +40,12 @@ public class AboutEnums {
     @Koan
     public void enumsWithAttributes() {
         // value is private but we still can access it. Why?
+        // because we are within the AboutEnums class, and can thus access inner-Enum and
+        // inner-Class variables.
         // Try moving the enum outside the AboutEnum class... What do you expect?
         // What happens?
-        assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, __);
+        // Unable to compile.  Unable to access private data.
+        assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, true);
     }
 
     enum OpticalMedia {
@@ -61,7 +64,7 @@ public class AboutEnums {
 
     @Koan
     public void enumsWithMethods() {
-        assertEquals(OpticalMedia.CD.getCoolnessFactor(), __);
-        assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), __);
+        assertEquals(OpticalMedia.CD.getCoolnessFactor(), (OpticalMedia.CD.capacityInMegaBytes - 1000) * 10);
+        assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), (OpticalMedia.BluRay.capacityInMegaBytes - 1000) * 10);
     }
 }
