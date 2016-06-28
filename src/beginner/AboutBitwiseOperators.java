8c02b46dd98714f1ae98c6e53024be560b08a6a0
Scott Fabini <sfabini@pdx.edu>
2016-06-27 17:14:39 -0700

    Completed Koans: AboutBitwiseOperators

--- src/beginner/AboutBitwiseOperators.java
+++ src/beginner/AboutBitwiseOperators.java
@@ -11,14 +11,16 @@ public class AboutBitwiseOperators {
     public void fullAnd() {
         int i = 1;
         if (true & (++i < 8)) i = i + 1;
-        assertEquals(i, __);
+        // the "true &" acts as a logical, non-short-circuited, AND operation
+        assertEquals(i, 3);
     }
 
     @Koan
     public void shortCircuitAnd() {
         int i = 1;
         if (true && (i < -28)) i = i + 1;
-        assertEquals(i, __);
+        // the "true &" is logical, but in now a short-circuited, AND operation
+        assertEquals(i, 1);
     }
 
     @Koan
@@ -26,40 +28,46 @@ public class AboutBitwiseOperators {
         int i = 1;
         int a = 6;
         if ((a < 9) ^ false) i = i + 1;
-        assertEquals(i, __);
+        // true ^ false = true.  Thus i = i + 1 is executed
+        assertEquals(i, 2);
     }
 
     @Koan
     public void dontMistakeEqualsForEqualsEquals() {
         int i = 1;
         boolean a = false;
-        if (a = true) i++;
-        assertEquals(a, __);
-        assertEquals(i, __);
+        if (true == a) i++;
+        assertEquals(a, false);
+        assertEquals(i, 1);
         // How could you write the condition 'with a twist' to avoid this trap?
+        // See above, swap so true == a.  true = a results in a compiler error, rather
+        // than just a warning
     }
 
     @Koan
     public void aboutBitShiftingRightShift() {
         int rightShift = 8;
         rightShift = rightShift >> 1;
-        assertEquals(rightShift, __);
+        assertEquals(rightShift, 4);
     }
 
     @Koan
     public void aboutBitShiftingLeftShift() {
         int leftShift = 0x80000000; // Is this number positive or negative?
         leftShift = leftShift << 1;
-        assertEquals(leftShift, __);
+        // << shifts the 1 off the end, yielding 0
+        assertEquals(leftShift, 0);
     }
 
     @Koan
     public void aboutBitShiftingRightUnsigned() {
         int rightShiftNegativeStaysNegative = 0x80000000;
         rightShiftNegativeStaysNegative = rightShiftNegativeStaysNegative >> 4;
-        assertEquals(rightShiftNegativeStaysNegative, __);
+        // >> fills with whatever the sign bit of 0x80000000 is (1)
+        assertEquals(rightShiftNegativeStaysNegative, 0xF8000000);
         int unsignedRightShift = 0x80000000; // always fills with 0
+        // >>> fills with 0s regardless of sign bit
         unsignedRightShift >>>= 4; // Just like +=
-        assertEquals(unsignedRightShift, __);
+        assertEquals(unsignedRightShift, 0x08000000);
     }
 }
