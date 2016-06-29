5c604bd6a7d79f640e584d92549168bfd3d93b2d
Scott Fabini <sfabini@pdx.edu>
2016-06-29 12:02:07 -0700

    Completed Koans: AboutJava7LiteralsEnhancements

--- src/java7/AboutJava7LiteralsEnhancements.java
+++ src/java7/AboutJava7LiteralsEnhancements.java
@@ -11,34 +11,34 @@ public class AboutJava7LiteralsEnhancements {
     public void binaryLiterals() {
         //binary literals are marked with 0b prefix
         short binaryLiteral = 0b1111;
-        assertEquals(binaryLiteral, __);
+        assertEquals(binaryLiteral, (short) 0xF);
     }
 
     @Koan
     public void binaryLiteralsWithUnderscores() {
         //literals can use underscores for improved readability
         short binaryLiteral = 0b1111_1111;
-        assertEquals(binaryLiteral, __);
+        assertEquals(binaryLiteral, (short) 0xFF);
     }
 
     @Koan
     public void numericLiteralsWithUnderscores() {
         long literal = 111_111_111L;
         //notice capital "B" - a valid binary literal prefix
-        short multiplier = 0B1_000;
-        assertEquals(literal * multiplier, __);
+        short multiplier = 0B1_000; // * 8
+        assertEquals(literal * multiplier, 888_888_888L);
     }
 
     @Koan
     public void negativeBinaryLiteral() {
         int negativeBinaryLiteral = 0b1111_1111_1111_1111_1111_1111_1111_1100 / 4;
-        assertEquals(negativeBinaryLiteral, __);
+        assertEquals(negativeBinaryLiteral, 0xFFFFFFFF);
     }
 
     @Koan
     public void binaryLiteralsWithBitwiseOperator() {
         int binaryLiteral = ~0b1111_1111;
-        assertEquals(binaryLiteral, __);
+        assertEquals(binaryLiteral, 0xFFFFFF00);
     }
 
 }
